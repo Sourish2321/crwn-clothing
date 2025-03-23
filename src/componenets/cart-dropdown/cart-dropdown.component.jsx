83dd875e5812cca8e2eccdbb320ac3e4d1213e5e
Sourish Polemoni <sourishtoast@gmail.com>
2025-03-23 12:21:19 +0530

    to sync with mac

--- src/componenets/cart-dropdown/cart-dropdown.component.jsx
+++ src/componenets/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,15 @@
 import Button from '../button/button.componenet'
 import './cart-dropdown.styles.scss'
 
+import CartItem from '../cart-item/cart-item.component'
+
 const CartDropdown = () => {
     return (
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
-             <Button>GO TO CHECKOUT</Button>   
+              [].map 
             </div>
+            <Button>GO TO CHECKOUT</Button>
         </div>
     )
 }
--- src/componenets/contexts/cart.context.jsx
+++ src/componenets/contexts/cart.context.jsx
@@ -1,12 +1,30 @@
 import { createContext, useState } from "react";
 
+const addCartItem = (cartItems, productToAdd) => {
+    const existingCartItem = cartItems.find((cartItems) => cartItems.id == productToAdd.id)
+    if (existingCartItem) {
+        return cartItems.map((cartItems) => cartItems.id == productToAdd.id ?
+        {...cartItems, quantity: cartItems.quantity + 1} 
+        : cartItems
+    )
+    }
+    return [...cartItems, {...productToAdd, quantity: 1}]
+}
+
+
 export const CartContext = createContext({
     isCartOpen: false,
-    setIsCartOpen: () => {}, 
+    setIsCartOpen: () => {},
+    cartItems: [],
+    setCartItems: () => {}
 });
 
 export const CartProvider = ({children}) => {
     const [isCartOpen, setIsCartOpen] = useState(false)
+    const [cartItems, setCartItems] = useState([])
+    const addItemToCart = (productToAdd) => {
+        setCartItems(addCartItem(cartItems, productToAdd))
+    }
     const value = {isCartOpen, setIsCartOpen}
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>
 }
