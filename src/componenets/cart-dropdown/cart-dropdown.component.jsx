491e9d98e9689e31d45c08107dd52ece4c6a2349
Sourish Polemoni <sourishtoast@gmail.com>
2025-03-24 18:31:15 +0530

    man what do u mean "addItemToCart" is a FUNCTION!!?!?

--- src/componenets/cart-dropdown/cart-dropdown.component.jsx
+++ src/componenets/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,17 @@
+import { useContext } from 'react'
+import { CartContext } from '../contexts/cart.context'
+
 import Button from '../button/button.componenet'
 import './cart-dropdown.styles.scss'
 
 import CartItem from '../cart-item/cart-item.component'
 
 const CartDropdown = () => {
+    const { cartItems } = useContext(CartContext)
     return (
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
-              [].map 
+              {cartItems.map(item => <CartItem key={item.id} cartItem={item} />)} 
             </div>
             <Button>GO TO CHECKOUT</Button>
         </div>
--- src/componenets/product-card/product-card.componenet.jsx
+++ src/componenets/product-card/product-card.componenet.jsx
@@ -1,17 +1,23 @@
+import { useContext } from 'react';
+import { CartContext } from '../contexts/cart.context';
+
 import './product-card.styles.scss'
 import Button from "../button/button.componenet"
  
 const ProductCard = ({ product }) => {
     const {name, price, imageUrl} = product;
+    const { addItemtoCart } = useContext(CartContext)
+
+    const addProductToCart = () => addItemtoCart(product);
     return (
         <div className='product-card-container'>
-        <img src={imageUrl} />
+        <img src={imageUrl} alt={name} />
         <div className='footer'>
             <span className='name'>{name}</span>
             
             <span className='price'>{price}</span>
         </div>
-        <Button buttonType='inverted'>Add to cart</Button>
+        <Button buttonType='inverted' onClick={addProductToCart}>Add to cart</Button>
     </div> 
     )
 }
